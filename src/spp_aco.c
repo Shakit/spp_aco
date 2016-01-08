35980a3b7f469b2b5f5bba518681d6c2fac4821f
shakit <dshakit@gmail.com>
2016-01-08 16:55:51 +0100

    refactoring + results

--- src/spp_aco.c
+++ src/spp_aco.c
@@ -12,9 +12,6 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-#define NBFOURMI 100
-#define EVAPORATION 0.4
-
 struct timeval start_utime, stop_utime;
 
 void crono_start()
@@ -39,6 +36,7 @@ double crono_ms()
     (stop_utime.tv_usec - start_utime.tv_usec) / 1000 ;
 }
 
+// data structure
 typedef struct
 {
 	int nbctr;
@@ -47,6 +45,7 @@ typedef struct
 	int* coef;
 } data;
 
+// array of int display procedure
 void display (const int* sol, const int size)
 {
 	int i;
@@ -57,6 +56,7 @@ void display (const int* sol, const int size)
 	printf("\n");
 }
 
+// array of double display procedure
 void displayd (const double* sol, const int size)
 {
 	int i;
@@ -67,12 +67,7 @@ void displayd (const double* sol, const int size)
 	printf("\n");
 }
 
-/* void antDisplay(int** fourmis) */
-/* { */
-/* 	for  */
-	
-/* } */
-
+// profit computation
 double cout(int* sol, data* dat)
 {
 	int i;
@@ -85,6 +80,7 @@ double cout(int* sol, data* dat)
 	return val;
 }
 
+// file reading function
 int readfile(data* dat, char* datafile)
 {
 	FILE *fin;
@@ -128,53 +124,77 @@ int readfile(data* dat, char* datafile)
 	return 1; 
 }
 
+//----------------------------------------------------------------------------------------------------
+// MAIN FUNCTION
 int main (int argc, char** argv)
 {
 	srand((unsigned int) time(0));
 	data dat;
-	int i,j,k,l; 
-	readfile(&dat, argv[1]);
+	int i,j,k,l;
+	int ITERMAX;
+	int NBFOURMI;
+	double EVAPORATION;
+	double random;
 
-	int** fourmis = (int **) malloc(sizeof(int*) * NBFOURMI);
+	double temps;
+
+	// parameters checking
+	if (argc != 5)
+	{
+		printf("error : usage : %s  <datafile> <nbiteration> <nbants> <evaporation>\n", argv[0]);
+		exit(1);
+	}
+	printf("datafile : %s\n", argv[1]);
+	printf("nbIterations : %s\n", argv[2]);
+	printf("nbAnts : %s\n", argv[3]);
+	printf("evaporation : %s\n", argv[4]);
+
+	ITERMAX = atoi(argv[2]);
+	NBFOURMI = atoi(argv[3]);
+	EVAPORATION = atof(argv[4]);
+
+	//file reading
+	readfile(&dat, argv[1]);
+	crono_start();
+
+	//initialisations
+	int* fourmis[NBFOURMI];
 	for (i = 0; i < NBFOURMI; i++)
 	{
 		fourmis[i] = (int *) malloc(sizeof(int) * dat.nbvar);
 	}
-	
 	double coutF[NBFOURMI];
 	for (i = 0; i < NBFOURMI; i++)
 	{
 		coutF[i] = 0;
 	}
-
-
 	double prob0[dat.nbvar];
 	for (i = 0; i < dat.nbvar; i++)
 	{
 		prob0[i] = NBFOURMI; 
 	}
-
 	double prob1[dat.nbvar];
 	for (i = 0; i < dat.nbvar; i++)
 	{
 		prob1[i] = NBFOURMI; 
 	}
-
 	double valOpti = 0;
-	double random;
-
 	int solOpti[dat.nbvar];
 	for (i = 0; i < dat.nbvar; i++)
 	{
 		solOpti[i] = 0;
 	}
 
-
-	int ITERMAX = atoi(argv[2]);
+	/* repeat
+	 *    - ants travelling
+	 *    - evaporation
+	 *    - evaluation
+	 *    - pheromone deposit
+	 * until ( iter == ITERMAX )	  
+	 */
 	int iter = 0;
 	while(iter < ITERMAX)
 	{
-	
 		for (j = 0; j < NBFOURMI; j++)
 		{
 			for (i = 0; i < dat.nbvar; i++)
@@ -186,10 +206,8 @@ int main (int argc, char** argv)
 			{
 				if (fourmis[j][i] == -1)
 				{
-					
-					
 					random = (rand() % (NBFOURMI * ITERMAX +1)) / (double) (NBFOURMI * ITERMAX + 1) * (prob0[i] + prob1[i]);
-		    
+
 					if (random < prob0[i])
 					{
 						fourmis[j][i] = 0;
@@ -204,18 +222,22 @@ int main (int argc, char** argv)
 								for (l = i+1; l < dat.nbvar; l++)
 								{
 									if (dat.matrix[k][l] == 1 ) fourmis[j][l] = 0;
-								} // end for
-							} // end if
-						} // end for
-					} // end if
- 				} // end if 
-			} // end for
-		} // end for
+								}
+							} 
+						} 
+					} 
+ 				} 
+			} // end for i (nbvar) 
+		} // end for j (fourmis) 
+
+		// evaporation
 		for (i = 0; i < dat.nbvar; i++)
 		{
 			prob0[i] *= EVAPORATION;
 			prob1[i] *= EVAPORATION;
 		}
+
+		// solutions evaluation 
 		for (i = 0; i < NBFOURMI; i++)
 		{
 			coutF[i] = cout(fourmis[i], &dat);
@@ -223,7 +245,6 @@ int main (int argc, char** argv)
 		double min = coutF[0];
 		double max = coutF[0];
 		int pos = 0;
-		
 		for (i = 1; i < NBFOURMI; i++)
 		{
 			if(min >coutF[i])
@@ -235,11 +256,7 @@ int main (int argc, char** argv)
 				max = coutF[i];
 				pos = i;
 			}
-		}
-
-		displayd(coutF, NBFOURMI);
-		printf("-----------------------------\n");
-		
+		}		
 		if(max > valOpti)
 		{
 			valOpti = max;
@@ -247,11 +264,13 @@ int main (int argc, char** argv)
 			{
 				solOpti[i] = fourmis[pos][i];
 			}
-			display(solOpti, dat.nbvar); 
-			
+			printf("\nBetter solution found ! \n");
+			printf("%f\n",valOpti);
+			display(solOpti, dat.nbvar);
+			printf("--------------------------------------------------\n");
 		}
 
-		
+		// pheromone deposit
 		if(min != max)
 		{	
 			for (i = 0; i < dat.nbvar; i++)
@@ -272,6 +291,26 @@ int main (int argc, char** argv)
 			}
 		}
 		iter++;
-		printf("\n%f\n",valOpti);
 	}// end while
+	
+	crono_stop();
+	temps = crono_ms()/1000,0;
+
+	// Solution display
+	printf("\n\nBest solution : \n");
+	printf("%f\n",valOpti);
+	display(solOpti, dat.nbvar);
+	printf("Computation time : %f\n", temps);
+	
+	for (i = 0; i < dat.nbctr; ++i)
+	{
+		free(dat.matrix[i]);
+	}
+	free(dat.matrix);
+	free(dat.coef);
+	for (i = 0; i < NBFOURMI; i++)
+	{
+		free(fourmis[i]);
+	}
+
 }
